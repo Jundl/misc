f1e8ee9c18dd6d26b577e108a1ee8e8c52f3ebd0
Gunther Polzin <gpolzin@gmx.net>
2016-07-07 13:28:46 +0200

    use arrow function in ES5

--- node_http_server/run_server.js
+++ node_http_server/run_server.js
@@ -3,8 +3,8 @@ var svr = express();
 svr.use(express.static('htdocs'));
 svr.listen(8081,'0.0.0.0');
 
-// etwas Dynamic:
-svr.get('/gettime', function(req, res){
+// etwas Dynamic .. geht sogar ohne transpiler
+svr.get('/gettime', (req, res)=>{
 	res.send('Mir haben\'s:<br><br> ' + (new Date()));
 });
 
